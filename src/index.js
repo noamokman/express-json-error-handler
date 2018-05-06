27698449ae4b4965372a7a2c201e1219729db104
Noam Okman <noamokman@gmail.com>
2018-05-06 20:14:35 +0300

    refactor(deps): update deps

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import httpStatus from 'http-status';
+import {INTERNAL_SERVER_ERROR, BAD_REQUEST} from 'http-status';
 import statuses from 'statuses';
 import _ from 'lodash';
 
-export default function ({log} = {}) {
-  return (err, req, res, next) => { // eslint-disable-line no-unused-vars
-    let status = err.status || err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+export default ({log} = {}) => (err, req, res, next) => { // eslint-disable-line no-unused-vars
+  let status = err.status || err.statusCode || INTERNAL_SERVER_ERROR;
 
-    if (status < httpStatus.BAD_REQUEST) {
-      status = httpStatus.INTERNAL_SERVER_ERROR;
-    }
+  if (status < BAD_REQUEST) {
+    status = INTERNAL_SERVER_ERROR;
+  }
 
-    res.status(status);
+  res.status(status);
 
-    const body = {
-      status
-    };
-
-    if (process.env.NODE_ENV !== 'production') {
-      body.stack = err.stack;
-    }
-
-    if (status >= httpStatus.INTERNAL_SERVER_ERROR) {
-      body.message = statuses[status];
-
-      if (log) {
-        log({err, req, res});
-      }
-      res.json(body);
-
-      return;
-    }
-
-    _.assign(body, _.pick(err, ['message', 'code', 'name', 'type']));
-
-    res.json(body);
+  const body = {
+    status
   };
-}
+
+  if (process.env.NODE_ENV !== 'production') {
+    body.stack = err.stack;
+  }
+
+  if (status >= INTERNAL_SERVER_ERROR) {
+    body.message = statuses[status];
+
+    if (log) {
+      log({err, req, res});
+    }
+    res.json(body);
+
+    return;
+  }
+
+  _.assign(body, _.pick(err, ['message', 'code', 'name', 'type']));
+
+  res.json(body);
+};
